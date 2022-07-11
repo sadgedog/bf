450ae440e7b4351c5c8780868378dca683ac436e
sadgedog <sorachi1231.214@gmail.com>
2022-07-11 19:04:05 +0900

    Add bf2c.cc

---
+++ bf2c.cc
@@ -0,0 +1,6 @@
+// brainfuck to C lang
+#include <iostream>
+
+int main(int argc, char **argv) {
+  return 0;
+}
--- bfi.cc
+++ bfi.cc
@@ -1,3 +1,4 @@
+// Brainfuck interpreter
 #include <iostream>
 using namespace std;
 
