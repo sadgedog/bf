80bdf011a078abc7a80c86b41e290a964f9028e7
sadgedog <sorachi1231.214@gmail.com>
2022-07-21 01:16:14 +0900

    png

---
+++ plot.py
@@ -0,0 +1,10 @@
+import matplotlib.pyplot as plt
+
+x = [115, 521, 1518, 2676, 11594]
+y = [0.276, 0.020, 0.046, 0.690, 82.096]
+yy = [0.105, 0.214, 0.248, 0.321, 0.853]
+
+plt.plot(x, y, label="Interpreter")
+plt.plot(x, yy, label="C translated")
+plt.legend()
+plt.show()
